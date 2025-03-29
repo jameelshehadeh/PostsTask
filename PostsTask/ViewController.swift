2a010d6dbe7b61a7d7c2c9942d600219433c3aa7
Jameel Shehadeh <jamshehadeh@gmail.com>
2025-03-29 14:26:03 +0400

    Initial Commit

---
+++ PostsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsTask
+//
+//  Created by Jameel Shehadeh on 29/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
