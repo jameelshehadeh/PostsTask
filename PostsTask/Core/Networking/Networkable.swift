f28df26ce457613a635aeecb847b870e341b2711
Jameel Shehadeh <jamshehadeh@gmail.com>
2025-04-01 19:16:54 +0400

    Add Networkable Protocol

---
+++ PostsTask/Core/Networking/Networkable.swift
@@ -0,0 +1,15 @@
+//
+//  Networkable.swift
+//  PostsTask
+//
+//  Created by Jameel Shehadeh on 01/04/2025.
+//
+
+import Foundation
+
+protocol Networkable {
+    
+    // Simulating networking calls 
+    func request<T: Decodable>(_ fileName: String, completion: @escaping (Result<T, Error>) -> Void)
+    
+}
