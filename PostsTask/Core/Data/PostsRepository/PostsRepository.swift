97271dc890f7bcbbd184ef25818afd7e43ef17f8
Jameel Shehadeh <jamshehadeh@gmail.com>
2025-04-01 19:22:01 +0400

    Add NetworkService Implementation

--- PostsTask/Core/Data/PostsRepository/PostsRepository.swift
+++ PostsTask/Core/Data/PostsRepository/PostsRepository.swift
@@ -9,9 +9,23 @@ import Foundation
 
 class PostsRepository: PostsRepositoryProtocol {
     
+    private let networkService: Networkable
+    
+    init(networkService: Networkable = NetworkService()) {
+        self.networkService = networkService
+    }
+    
     func fetchPosts(completion: @escaping (Result<[Post], any Error>) -> ()) {
         
-    
+        networkService.request("posts") { (result: Result<[Post], Error>) in
+            switch result {
+            case .success(let posts):
+                completion(.success(posts))
+            case .failure(let failure):
+                completion(.failure(failure))
+            }
+        }
+        
     }
     
 }
---
+++ PostsTask/Core/Networking/NetworkService.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkService.swift
+//  PostsTask
+//
+//  Created by Jameel Shehadeh on 01/04/2025.
+//
+
+import Foundation
+
+class NetworkService: Networkable {
+    
+    func request<T>(_ fileName: String, completion: @escaping (Result<T, any Error>) -> Void) where T : Decodable {
+        
+        // Simulate network delay
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1.5) {
+            guard let url = Bundle.main.url(forResource: fileName, withExtension: "json") else {
+                completion(.failure(URLError(.badURL)))
+                return
+            }
+            do {
+                let data = try Data(contentsOf: url)
+                let model = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(model))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+    }
+    
+}
